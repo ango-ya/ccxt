02ff87f7695452b4daba17c69a95f524edd13d25
yuta-mine <jamyukirom@gmail.com>
2025-02-07 19:40:32 +0900

    add callLoadMarket in bitmart

--- ts/src/bitmart.ts
+++ ts/src/bitmart.ts
@@ -35,6 +35,7 @@ export default class bitmart extends Exchange {
                 'option': false,
                 'borrowCrossMargin': false,
                 'borrowIsolatedMargin': true,
+                'callLoadMarkets': true,
                 'cancelAllOrders': true,
                 'cancelOrder': true,
                 'cancelOrders': true,
@@ -839,6 +840,16 @@ export default class bitmart extends Exchange {
                 },
             },
         });
+    }
+
+    async callLoadMarkets (coinListData = undefined, marketData = undefined) {
+        /**
+         * @method
+         * @name bitget#callLoadMarkets
+         * @description call fetch currencies and fetch markets
+         * @param {object} data extra parameters specific to the bitget api endpoint
+         */
+        await this.loadMarkets (coinListData, marketData);
     }
 
     async fetchTime (params = {}) {
