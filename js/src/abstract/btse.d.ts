63aad91fcf1c9dddb6dafdd043b12a378184aee6
yuta-mine <jamyukirom@gmail.com>
2025-07-08 15:37:58 +0900

    build and version-up

---
+++ js/src/abstract/btse.d.ts
@@ -0,0 +1,25 @@
+import { implicitReturnType } from '../base/types.js';
+import { Exchange as _Exchange } from '../base/Exchange.js';
+interface Exchange {
+    publicGetApiV32MarketSummary(params?: {}): Promise<implicitReturnType>;
+    publicGetApiV32Orderbook(params?: {}): Promise<implicitReturnType>;
+    publicGetApiV32Trades(params?: {}): Promise<implicitReturnType>;
+    publicGetApiV32Ohlcv(params?: {}): Promise<implicitReturnType>;
+    publicGetApiV32MarketSummarySymbol(params?: {}): Promise<implicitReturnType>;
+    publicGetApiV32OrderbookSymbol(params?: {}): Promise<implicitReturnType>;
+    publicGetApiV32TradesSymbol(params?: {}): Promise<implicitReturnType>;
+    publicGetApiV32OhlcvSymbol(params?: {}): Promise<implicitReturnType>;
+    privateGetApiV32UserWallet(params?: {}): Promise<implicitReturnType>;
+    privateGetApiV32UserWalletSymbol(params?: {}): Promise<implicitReturnType>;
+    privateGetApiV32UserOpenOrders(params?: {}): Promise<implicitReturnType>;
+    privateGetApiV32UserOrders(params?: {}): Promise<implicitReturnType>;
+    privateGetApiV32UserOrderOrderId(params?: {}): Promise<implicitReturnType>;
+    privateGetApiV32UserTrades(params?: {}): Promise<implicitReturnType>;
+    privateGetApiV32UserTradesSymbol(params?: {}): Promise<implicitReturnType>;
+    privatePostApiV32Order(params?: {}): Promise<implicitReturnType>;
+    privateDeleteApiV32Order(params?: {}): Promise<implicitReturnType>;
+    privateDeleteApiV32OrderOrderId(params?: {}): Promise<implicitReturnType>;
+}
+declare abstract class Exchange extends _Exchange {
+}
+export default Exchange;
