3b7287150c5b88fa37a761753c78c53e0460f9bd
yuta-mine <jamyukirom@gmail.com>
2025-07-08 16:22:48 +0900

    fix endpoint

--- ts/src/btse.ts
+++ ts/src/btse.ts
@@ -138,19 +138,15 @@ export default class btse extends Exchange {
                 'private': {
                     'get': [
                         'api/v3.2/user/wallet',
-                        'api/v3.2/user/wallet/{symbol}',
                         'api/v3.2/user/open_orders',
-                        'api/v3.2/user/orders',
-                        'api/v3.2/user/order/{order_id}',
-                        'api/v3.2/user/trades',
-                        'api/v3.2/user/trades/{symbol}',
+                        'api/v3.2/order',
+                        'api/v3.2/user/trade_history',
                     ],
                     'post': [
                         'api/v3.2/order',
                     ],
                     'delete': [
                         'api/v3.2/order',
-                        'api/v3.2/order/{order_id}',
                     ],
                 },
             },
@@ -199,11 +195,6 @@ export default class btse extends Exchange {
                 'secret': true,
             },
         });
-    }
-
-    async loadMarkets (reload = false, params = {}) {
-        const markets = await super.loadMarkets (reload, params);
-        return markets;
     }
 
     async fetchMarkets (params = {}) {
